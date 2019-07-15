36e6d2575a0ce915a9bd11ce97fce515dd145b11
sebastianacevedoc <sebastian.acevedoc@usach.cl>
2019-07-14 23:32:30 -0400

    change case test

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -3,7 +3,7 @@ from app import my_function
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(my_function(1, 1), 2)
+        self.assertEqual(my_function(1, 1), 3)
         self.assertEqual(my_function(1, -1), 0)
         self.assertEqual(my_function(0, 0), 0)
         self.assertEqual(my_function(-1, -1), -2)
