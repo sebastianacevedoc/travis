e4cce5ced15b59c2462024cbf3eedb45e355e09b
sebastianacevedoc <sebastian.acevedoc@usach.cl>
2019-07-14 23:39:07 -0400

    fix test case

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -9,7 +9,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(my_function(-1, -1), -2)
         self.assertEqual(my_function(1.0, 1), 2)        
         self.assertEqual(my_function(1.1, 1.1), 2.2)
-	self.assertEqual(my_function(3, 4), 7)       
+        self.assertEqual(my_function(3, 4), 7)
 
 if __name__ == '__main__':
     unittest.main()
