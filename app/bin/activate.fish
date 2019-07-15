b96b92f93fe0232ecd7be3f847c93126eae55f07
sebastianacevedoc <sebastian.acevedoc@usach.cl>
2019-07-14 23:08:04 -0400

    add env

--- app/bin/activate.fish
+++ app/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/seba/Desktop/app"
+set -gx VIRTUAL_ENV "/Users/seba/Desktop/travis/app"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
